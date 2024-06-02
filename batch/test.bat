00cbde422ad2e2807787521d5f9bdb3e562deaf7
qwk <80052118+qwkdev@users.noreply.github.com>
2024-06-02 23:19:53 +0100

    Update test.bat

--- batch/test.bat
+++ batch/test.bat
@@ -2,3 +2,10 @@
 echo Testing TTS...
 pause
 powershell -command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Text To Speech Works');"
+cls
+echo Testing Browser...
+pause
+taskkill /im chrome.exe /f
+start https://google.com/
+start https://example.com/
+taskkill /im chrome.exe /f

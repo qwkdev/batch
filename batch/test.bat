c04b7ef91ff7acf76cbbc52b5be168758edb86c9
qwk <80052118+qwkdev@users.noreply.github.com>
2024-06-03 23:14:20 +0100

    Update test.bat

--- batch/test.bat
+++ batch/test.bat
@@ -9,3 +9,17 @@ taskkill /im chrome.exe /f
 start https://google.com/
 start https://example.com/
 taskkill /im chrome.exe /f
+cls
+echo Testing fsutil...
+pause
+fsutil file createnew i_should_be_69_kb.txt 70656
+start .
+cls
+echo Testing xcopy...
+pause
+mkdir "test_xcopy_dir"
+cd "test_xcopy_dir"
+fsutil file createnew i_should_be_69_kb.txt 70656
+cd ..
+xcopy "test_xcopy_dir" "test_xcopy_dir_COPY" /E /I > NUL 2>&1
+start .

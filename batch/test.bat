75373cada857e4f374c8332ece3a635dd07ee899
qwk <80052118+qwkdev@users.noreply.github.com>
2024-06-11 23:26:30 +0100

    Update test.bat

--- batch/test.bat
+++ batch/test.bat
@@ -1,25 +1,15 @@
 @echo off
-echo Testing TTS...
+:: Attempt to echo %CD%
+setlocal EnableDelayedExpansion
+set "currentDir=%CD%"
+
+:: Check if the current directory variable is empty
+if "!currentDir!"=="" (
+    echo The script is not allowed to know the current directory.
+) else (
+    echo The current directory is: %currentDir%
+    :: You can proceed with your actions here
+)
+endlocal
 pause
-powershell -command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Text To Speech Works');"
-cls
-echo Testing Browser...
-pause
-taskkill /im chrome.exe /f
-start https://google.com/
-start https://example.com/
-taskkill /im chrome.exe /f
-cls
-echo Testing fsutil...
-pause
-fsutil file createnew i_should_be_69_kb.txt 70656
-start .
-cls
-echo Testing xcopy...
-pause
-mkdir "test_xcopy_dir"
-cd "test_xcopy_dir"
-fsutil file createnew i_should_be_69_kb.txt 70656
-cd ..
-xcopy "test_xcopy_dir" "test_xcopy_dir_COPY" /E /I > NUL 2>&1
-start .
+
